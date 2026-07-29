29355b770d276fd306161dd4639266a0c63e4f7d
Debmallar Dasgupta <debmallardasgupta@gmail.com>
2026-07-29 23:42:19 +0530

    feat : create 3330.cpp solution

---
+++ topics/String/3330/3330.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int ans = 1,cnt = 1, n = word.size();
+        for (int i = 1; i < n; i++) {
+            if (word[i] == word[i - 1]) {
+                cnt++;
+            } else {
+                ans += cnt - 1;
+                cnt = 1;
+            }
+        }
+        ans += cnt - 1;
+        return ans;
+    }
+};
