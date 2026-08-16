6409faf1bea223d11ee78e6808b6711476f65319
Debmallar Dasgupta <debmallardasgupta@gmail.com>
2026-08-16 09:05:46 +0530

    Create 9.cpp

---
+++ topics/Maths/9/9.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        long long rev = 0;
+        int temp = x;
+        while(x > 0) {
+            rev = rev * 10 * 1LL + x % 10;
+            x /= 10;
+        }
+        return rev == temp;
+    }
+};
