d1347831ed7d4da3b17c7915d4a868c0569f725a
Debmallar Dasgupta <debmallardasgupta@gmail.com>
2026-08-18 19:18:45 +0530

    Create 268.cpp

---
+++ topics/Arrays & Hashing/arrays/268/268.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> vis(n + 1, 0);
+        for(int i = 0; i < n; i++) {
+            vis[nums[i]] = 1;
+        }
+        for(int i = 0; i <= n; i++) {
+            if(vis[i] != 1) return i;
+        }
+        return -1;
+    }
+};
