3a740ee4493c932e5a02e13e2a37cdc67314ec21
Debmallar Dasgupta <debmallardasgupta@gmail.com>
2026-08-19 23:28:47 +0530

    Create 1.cpp

---
+++ topics/Arrays & Hashing/arrays/1/1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            hash[nums[i]] = i;
+        }
+        for(int i = 0; i < n; i++) {
+            int comp = target - nums[i];
+            if(hash.find(comp) != hash.end() && hash[comp] != i) return {i, hash[comp]};
+        }
+        return {};
+    }
+};
