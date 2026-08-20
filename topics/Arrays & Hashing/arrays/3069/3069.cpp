3c6da8221d6e9951dc3f35e4cf736a32733bcc54
Debmallar Dasgupta <debmallardasgupta@gmail.com>
2026-08-20 23:52:46 +0530

    Create 3069.cpp

---
+++ topics/Arrays & Hashing/arrays/3069/3069.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> a, b;
+        a = {nums[0]};
+        b = {nums[1]};
+        int n = nums.size();
+        for(int i = 2; i < n; i++){
+            if(a.back() > b.back()) a.push_back(nums[i]);
+            else b.push_back(nums[i]);
+        }
+        for(auto it : b) a.push_back(it);
+        return a;
+    }
+};
